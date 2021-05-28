fc2c2154e1d1f52e2158f9ed8f84730ea7bf05a7
Dawuda Nuraini <lawsondawuda6@gmail.com>
2021-05-28 09:35:25 +0000

    A set of voices that plays upon on click

---
+++ app/src/main/java/andriod/bignerdranch/myfirstbeatbox/BeatBoxActivity.java
@@ -0,0 +1,12 @@
+package andriod.bignerdranch.myfirstbeatbox;
+
+
+import androidx.fragment.app.Fragment;
+
+public class BeatBoxActivity extends
+       SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return BeatBoxFragment.newInstance();
+    }
+}
---
+++ app/src/main/java/andriod/bignerdranch/myfirstbeatbox/SingleFragmentActivity.java
@@ -0,0 +1,48 @@
+package andriod.bignerdranch.myfirstbeatbox;
+
+import android.os.Bundle;
+
+import androidx.annotation.LayoutRes;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    protected abstract Fragment createFragment();
+
+
+    @LayoutRes
+    protected int getLayoutResId() {
+        return R.layout.activity_crime;
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutResId());
+
+        beginFragmentJoin();
+    }
+
+
+
+
+
+
+    public void beginFragmentJoin () {
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment =
+                fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = createFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+    }
+
+}
